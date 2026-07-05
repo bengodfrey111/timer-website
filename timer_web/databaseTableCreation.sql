ede92a246ceadf677bbd4bf899b169e20b977ee6
user <bengodfrey111@gmail.com>
2026-07-05 21:16:00 +0100

    better salting

--- timer_web/databaseTableCreation.sql
+++ timer_web/databaseTableCreation.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "timers" (
 	"name"	TEXT UNIQUE,
 	"password"	TEXT,
+	"password_salt"	TEXT,
 	"endTime"	INTEGER,
 	PRIMARY KEY("name")
 );
--- timer_web/timerStorage.py
+++ timer_web/timerStorage.py
@@ -4,9 +4,10 @@ import math
 import hashlib
 import usefulFunctions
 import time
+import uuid
 
-def hashPassword(name, password): #this hashes the password, learnt in https://docs.python.org/3/library/hashlib.html
-    return str(hashlib.sha512(b"c6-wY9$Ka3zh[c$#m[CHaTAF_f&WZ&GCwM$Yq6Jg" + str(name).encode() + str(password).encode()).hexdigest())
+def hashPassword(salt, password): #this hashes the password, learnt in https://docs.python.org/3/library/hashlib.html
+    return str(hashlib.sha512(str(salt).encode() + str(password).encode()).hexdigest())
 
 def databaseLoc():
     return "timerDB.db"
@@ -17,17 +18,19 @@ class timer:
         self.exist = False
         self.endTime = None
         self.password = None
+        self.password_salt = None
 
         #password = hashPassword(self.name, password) #duplicate of controlRoom function that checks if credentials are correct
         connection = sqlite3.connect(databaseLoc())
-        cursor = connection.execute("SELECT * FROM timers WHERE name=:name", {"name": name})
+        cursor = connection.execute("SELECT password, password_salt, endTime FROM timers WHERE name=:name", {"name": name})
         for row in cursor:
             self.exist = True
             self.endTime = int(row[2]) #has to be int since sqllite does not support decimals (will lose fraction of millisecond so doesn't matter)
-            self.password = row[1]
+            self.password = row[0]
+            self.password_salt = row[1]
         connection.close()
     def controlTimer(self, password):
-        return self.password == hashPassword(self.name, password)
+        return self.password == hashPassword(self.password_salt, password)
 
 
     def removeTimer(self): #removes the timer
@@ -49,8 +52,9 @@ class timer:
 
 
 def newTimer(name, password): #creates the timer
+    salt = uuid.uuid4().hex
     connection = sqlite3.connect(databaseLoc())
-    connection.execute("INSERT INTO timers (name, password, endTime) VALUES (:name, :password, :endTime)", {"name": name, "password": hashPassword(name, password), "endTime": math.ceil(time.time() * 1000)}) #datetime is stored as unix time to prevent timezone issues
+    connection.execute("INSERT INTO timers (name, password, password_salt, endTime) VALUES (:name, :password, :password_salt, :endTime)", {"name": name, "password": hashPassword(salt, password), "password_salt": salt, "endTime": math.ceil(time.time() * 1000)}) #datetime is stored as unix time to prevent timezone issues
     connection.commit()
     connection.close()
     return timer(name)
